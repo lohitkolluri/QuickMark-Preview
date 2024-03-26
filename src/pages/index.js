3033212b091cda7be1be55f187e5f3120c797d3b
Lohit Kolluri <lohitkolluri@gmail.com>
2024-03-26 21:30:11 +0530

    🎨 Add Favicon and Description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Head from 'next/head';
 import Markdown from '../pages/Markdown';
 
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>QuickMark Preview</title>
+        <meta name="description" content="Live Markdown Editor" />
+        <link type="image/png" sizes="96×96" rel="icon" href="/favicon.png" />
+      </Head>
       <Markdown />
     </div>
   );
